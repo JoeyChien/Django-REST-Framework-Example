a881f6eb423d58ef8bab131a6edc1ad8c3e2fffe
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-03-09 22:28:48 +0800

    安裝 REST framework，將 db 改為使用 postgresql，並且安裝 dotenv 套件來把資料庫的連線資訊改用環境變數方式帶入

--- myRESTAPI/settings.py
+++ myRESTAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todolistapi',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'API.urls'
+ROOT_URLCONF = 'myRESTAPI.urls'
 
 TEMPLATES = [
     {
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'myRESTAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restfulAPI',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PWD'),
+        'HOST': os.getenv('DB_HOST')
     }
 }
 
